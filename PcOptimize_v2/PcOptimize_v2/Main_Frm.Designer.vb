47fed4faa8cef31e4bd58c90a4dfb521988e26f0
Semih Silistre <semihsilistre@365office.site>
2018-10-29 01:20:30 +0300

    News Update

--- PcOptimize_v2/PcOptimize_v2/Main_Frm.Designer.vb
+++ PcOptimize_v2/PcOptimize_v2/Main_Frm.Designer.vb
@@ -22,16 +22,147 @@ Partial Class Main_Frm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MetroCheckBox1 = New MetroFramework.Controls.MetroCheckBox()
+        Me.MetroCheckBox2 = New MetroFramework.Controls.MetroCheckBox()
+        Me.MetroCheckBox3 = New MetroFramework.Controls.MetroCheckBox()
+        Me.MetroCheckBox4 = New MetroFramework.Controls.MetroCheckBox()
+        Me.MetroCheckBox5 = New MetroFramework.Controls.MetroCheckBox()
+        Me.MetroCheckBox6 = New MetroFramework.Controls.MetroCheckBox()
+        Me.MetroCheckBox7 = New MetroFramework.Controls.MetroCheckBox()
+        Me.MetroCheckBox8 = New MetroFramework.Controls.MetroCheckBox()
         Me.SuspendLayout()
+        '
+        'MetroCheckBox1
+        '
+        Me.MetroCheckBox1.AutoSize = True
+        Me.MetroCheckBox1.FontSize = MetroFramework.MetroCheckBoxSize.Medium
+        Me.MetroCheckBox1.FontWeight = MetroFramework.MetroCheckBoxWeight.Bold
+        Me.MetroCheckBox1.Location = New System.Drawing.Point(12, 92)
+        Me.MetroCheckBox1.Name = "MetroCheckBox1"
+        Me.MetroCheckBox1.Size = New System.Drawing.Size(145, 20)
+        Me.MetroCheckBox1.TabIndex = 0
+        Me.MetroCheckBox1.Text = "MetroCheckBox1"
+        Me.MetroCheckBox1.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroCheckBox1.UseSelectable = True
+        '
+        'MetroCheckBox2
+        '
+        Me.MetroCheckBox2.AutoSize = True
+        Me.MetroCheckBox2.FontSize = MetroFramework.MetroCheckBoxSize.Medium
+        Me.MetroCheckBox2.FontWeight = MetroFramework.MetroCheckBoxWeight.Bold
+        Me.MetroCheckBox2.Location = New System.Drawing.Point(12, 118)
+        Me.MetroCheckBox2.Name = "MetroCheckBox2"
+        Me.MetroCheckBox2.Size = New System.Drawing.Size(145, 20)
+        Me.MetroCheckBox2.TabIndex = 0
+        Me.MetroCheckBox2.Text = "MetroCheckBox1"
+        Me.MetroCheckBox2.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroCheckBox2.UseSelectable = True
+        '
+        'MetroCheckBox3
+        '
+        Me.MetroCheckBox3.AutoSize = True
+        Me.MetroCheckBox3.FontSize = MetroFramework.MetroCheckBoxSize.Medium
+        Me.MetroCheckBox3.FontWeight = MetroFramework.MetroCheckBoxWeight.Bold
+        Me.MetroCheckBox3.Location = New System.Drawing.Point(12, 144)
+        Me.MetroCheckBox3.Name = "MetroCheckBox3"
+        Me.MetroCheckBox3.Size = New System.Drawing.Size(145, 20)
+        Me.MetroCheckBox3.TabIndex = 0
+        Me.MetroCheckBox3.Text = "MetroCheckBox1"
+        Me.MetroCheckBox3.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroCheckBox3.UseSelectable = True
+        '
+        'MetroCheckBox4
+        '
+        Me.MetroCheckBox4.AutoSize = True
+        Me.MetroCheckBox4.FontSize = MetroFramework.MetroCheckBoxSize.Medium
+        Me.MetroCheckBox4.FontWeight = MetroFramework.MetroCheckBoxWeight.Bold
+        Me.MetroCheckBox4.Location = New System.Drawing.Point(12, 170)
+        Me.MetroCheckBox4.Name = "MetroCheckBox4"
+        Me.MetroCheckBox4.Size = New System.Drawing.Size(145, 20)
+        Me.MetroCheckBox4.TabIndex = 0
+        Me.MetroCheckBox4.Text = "MetroCheckBox1"
+        Me.MetroCheckBox4.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroCheckBox4.UseSelectable = True
+        '
+        'MetroCheckBox5
+        '
+        Me.MetroCheckBox5.AutoSize = True
+        Me.MetroCheckBox5.FontSize = MetroFramework.MetroCheckBoxSize.Medium
+        Me.MetroCheckBox5.FontWeight = MetroFramework.MetroCheckBoxWeight.Bold
+        Me.MetroCheckBox5.Location = New System.Drawing.Point(215, 92)
+        Me.MetroCheckBox5.Name = "MetroCheckBox5"
+        Me.MetroCheckBox5.Size = New System.Drawing.Size(145, 20)
+        Me.MetroCheckBox5.TabIndex = 0
+        Me.MetroCheckBox5.Text = "MetroCheckBox1"
+        Me.MetroCheckBox5.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroCheckBox5.UseSelectable = True
+        '
+        'MetroCheckBox6
+        '
+        Me.MetroCheckBox6.AutoSize = True
+        Me.MetroCheckBox6.FontSize = MetroFramework.MetroCheckBoxSize.Medium
+        Me.MetroCheckBox6.FontWeight = MetroFramework.MetroCheckBoxWeight.Bold
+        Me.MetroCheckBox6.Location = New System.Drawing.Point(215, 118)
+        Me.MetroCheckBox6.Name = "MetroCheckBox6"
+        Me.MetroCheckBox6.Size = New System.Drawing.Size(145, 20)
+        Me.MetroCheckBox6.TabIndex = 0
+        Me.MetroCheckBox6.Text = "MetroCheckBox1"
+        Me.MetroCheckBox6.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroCheckBox6.UseSelectable = True
+        '
+        'MetroCheckBox7
+        '
+        Me.MetroCheckBox7.AutoSize = True
+        Me.MetroCheckBox7.FontSize = MetroFramework.MetroCheckBoxSize.Medium
+        Me.MetroCheckBox7.FontWeight = MetroFramework.MetroCheckBoxWeight.Bold
+        Me.MetroCheckBox7.Location = New System.Drawing.Point(215, 144)
+        Me.MetroCheckBox7.Name = "MetroCheckBox7"
+        Me.MetroCheckBox7.Size = New System.Drawing.Size(145, 20)
+        Me.MetroCheckBox7.TabIndex = 0
+        Me.MetroCheckBox7.Text = "MetroCheckBox1"
+        Me.MetroCheckBox7.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroCheckBox7.UseSelectable = True
+        '
+        'MetroCheckBox8
+        '
+        Me.MetroCheckBox8.AutoSize = True
+        Me.MetroCheckBox8.FontSize = MetroFramework.MetroCheckBoxSize.Medium
+        Me.MetroCheckBox8.FontWeight = MetroFramework.MetroCheckBoxWeight.Bold
+        Me.MetroCheckBox8.Location = New System.Drawing.Point(215, 170)
+        Me.MetroCheckBox8.Name = "MetroCheckBox8"
+        Me.MetroCheckBox8.Size = New System.Drawing.Size(145, 20)
+        Me.MetroCheckBox8.TabIndex = 0
+        Me.MetroCheckBox8.Text = "MetroCheckBox1"
+        Me.MetroCheckBox8.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroCheckBox8.UseSelectable = True
         '
         'Main_Frm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(733, 568)
+        Me.Controls.Add(Me.MetroCheckBox5)
+        Me.Controls.Add(Me.MetroCheckBox8)
+        Me.Controls.Add(Me.MetroCheckBox4)
+        Me.Controls.Add(Me.MetroCheckBox7)
+        Me.Controls.Add(Me.MetroCheckBox3)
+        Me.Controls.Add(Me.MetroCheckBox6)
+        Me.Controls.Add(Me.MetroCheckBox2)
+        Me.Controls.Add(Me.MetroCheckBox1)
         Me.Name = "Main_Frm"
         Me.Text = "Main_Frm"
+        Me.Theme = MetroFramework.MetroThemeStyle.Dark
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents MetroCheckBox1 As MetroFramework.Controls.MetroCheckBox
+    Friend WithEvents MetroCheckBox2 As MetroFramework.Controls.MetroCheckBox
+    Friend WithEvents MetroCheckBox3 As MetroFramework.Controls.MetroCheckBox
+    Friend WithEvents MetroCheckBox4 As MetroFramework.Controls.MetroCheckBox
+    Friend WithEvents MetroCheckBox5 As MetroFramework.Controls.MetroCheckBox
+    Friend WithEvents MetroCheckBox6 As MetroFramework.Controls.MetroCheckBox
+    Friend WithEvents MetroCheckBox7 As MetroFramework.Controls.MetroCheckBox
+    Friend WithEvents MetroCheckBox8 As MetroFramework.Controls.MetroCheckBox
 End Class
